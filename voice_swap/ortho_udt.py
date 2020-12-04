23e4ddfcc6b5a9465f3985c339cc3d4b9ceae50b
Alex Nichol <unixpickle@gmail.com>
2020-12-03 19:21:38 -0500

    use GPU for neighbor calcs with PyTorch if possible

--- voice_swap/ortho_udt.py
+++ voice_swap/ortho_udt.py
@@ -66,6 +66,16 @@ def nearest_neighbors(source, target, batch_size=128, verbose=False):
              integers, giving the index of a target or source vector for each
              source or target vector.
     """
+    try:
+        import torch
+
+        if torch.cuda.is_available():
+            return _nearest_neighbors_torch(
+                source, target, batch_size=batch_size, verbose=verbose
+            )
+    except ImportError:
+        pass
+
     indices = np.zeros([len(source)], dtype=np.int)
     distances = np.inf * np.ones([len(source)], dtype=source.dtype)
     target_indices = []
@@ -84,3 +94,29 @@ def nearest_neighbors(source, target, batch_size=128, verbose=False):
         distances = np.minimum(distances, min_values)
         target_indices.append(np.argmin(distance_mat, axis=0))
     return indices, np.concatenate(target_indices, axis=0)
+
+
+def _nearest_neighbors_torch(source, target, batch_size=128, verbose=False):
+    import torch
+
+    dev = torch.device("cuda")
+    source = torch.from_numpy(source).to(torch.float32).to(dev)
+    target = torch.from_numpy(target).to(source)
+    indices = torch.zeros([len(source)], device=dev, dtype=torch.int64)
+    distances = (torch.ones([len(source)]) * np.inf).to(source)
+    target_indices = []
+    batches = range(0, len(target), batch_size)
+    if verbose:
+        batches = tqdm(batches)
+    for i in batches:
+        batch = target[i : i + batch_size]
+        source_norms = torch.sum(source * source, dim=-1)[:, None]
+        target_norms = torch.sum(batch * batch, dim=-1)[None]
+        dots = source @ batch.T
+        distance_mat = source_norms + target_norms - 2 * dots
+        min_indices = torch.argmin(distance_mat, dim=-1)
+        min_values, _ = torch.min(distance_mat, dim=-1)
+        indices = torch.where(min_values < distances, min_indices + i, indices)
+        distances = torch.where(min_values < distances, min_values, distances)
+        target_indices.append(torch.argmin(distance_mat, dim=0).cpu().numpy())
+    return indices.cpu().numpy(), np.concatenate(target_indices, axis=0)
