ca982bb03d8526562010e46293721354abd700c4
Alex Nichol <unixpickle@gmail.com>
2020-12-05 14:12:39 -0500

    cleanup neighbor calculator

--- voice_swap/ortho_udt.py
+++ voice_swap/ortho_udt.py
@@ -3,6 +3,8 @@ Orthogonal unsupervised domain translation, as implemented by
 https://arxiv.org/abs/2007.12568.
 """
 
+from abc import ABC, abstractmethod
+
 import numpy as np
 from tqdm.auto import tqdm
 
@@ -20,20 +22,21 @@ def ortho_udt(source, target, verbose=False, no_cycle_check=False, max_iters=Non
     :return: a [D x D] orthogonal matrix that takes vectors from the source
              and produces vectors from the target.
     """
+    neighbor_calc = Neighbors.create(verbose=verbose)
     solution = np.eye(source.shape[1])
     num_iters = 0
     while True:
         new_source = source @ solution
         if verbose:
             print("Computing neighbors...")
-        source_neighbors, target_neighbors = nearest_neighbors(
+        source_neighbors, target_neighbors = neighbor_calc.neighbors(
             new_source, target, verbose=verbose
         )
 
-        use_sources = target_neighbors[source_neighbors] == np.arange(len(new_source))
+        best_buddies = target_neighbors[source_neighbors] == np.arange(len(new_source))
 
         stats = {
-            "best_buddies": np.sum(use_sources),
+            "best_buddies": np.sum(best_buddies),
             "uniq_targets": len(set(source_neighbors)),
             "uniq_sources": len(set(target_neighbors)),
         }
@@ -44,8 +47,8 @@ def ortho_udt(source, target, verbose=False, no_cycle_check=False, max_iters=Non
                 [target[source_neighbors], source[target_neighbors]], axis=0
             )
         else:
-            source_vecs = source[use_sources]
-            target_vecs = target[source_neighbors[use_sources]]
+            source_vecs = source[best_buddies]
+            target_vecs = target[source_neighbors[best_buddies]]
 
         stats["mse"] = np.mean((source_vecs - target_vecs) ** 2)
 
@@ -71,66 +74,85 @@ def ortho_udt(source, target, verbose=False, no_cycle_check=False, max_iters=Non
     return solution
 
 
-def nearest_neighbors(source, target, batch_size=128, verbose=False):
+class Neighbors(ABC):
     """
-    For each source vector, compute the target vector that is nearest to the
-    source vector, and vice versa.
+    A k-Nearest Neighbor calculator.
+    """
 
-    :return: a tuple (source_indices, target_indices), both 1-D arrays of
-             integers, giving the index of a target or source vector for each
-             source or target vector.
-    """
-    try:
+    def __init__(self, verbose=False, batch_size=128):
+        self.verbose = verbose
+        self.batch_size = batch_size
+
+    @staticmethod
+    def create(verbose=False, batch_size=128):
+        try:
+            import torch
+
+            if torch.cuda.is_available():
+                return TorchNeighbors(verbose, batch_size)
+        except ModuleNotFoundError:
+            return NumpyNeighbors(verbose, batch_size)
+
+    @abstractmethod
+    def neighbors(self, a, b):
+        """
+        For each vector in arrays a and b, compute the nearest vector in the
+        other array.
+
+        :param a: a 2-D numpy array of vectors.
+        :param b: a 2-D numpy array of vectors.
+        :return: a tuple (a_neighbors, b_neighbors), where each element of
+        a_neighbors is an index in b, and vice versa.
+        """
+
+
+class NumpyNeighbors(Neighbors):
+    def neighbors(self, a, b):
+        a_neighbors = np.zeros([len(a)], dtype=np.int)
+        b_neighbors = []
+        a_distances = np.inf * np.ones([len(a)], dtype=a.dtype)
+        batches = range(0, len(b), self.batch_size)
+        if self.verbose:
+            batches = tqdm(batches)
+        a_norms = np.sum(a ** 2, axis=-1)[:, None]
+        for i in batches:
+            batch = b[i : i + self.batch_size]
+            batch_norms = np.sum(batch ** 2, axis=-1)[None]
+            dots = a @ batch.T
+            distance_mat = a_norms + batch_norms - 2 * dots
+            min_indices = np.argmin(distance_mat, axis=-1)
+            min_values = np.min(distance_mat, axis=-1)
+            a_neighbors = np.where(
+                min_values < a_distances, min_indices + i, a_neighbors
+            )
+            a_distances = np.minimum(a_distances, min_values)
+            b_neighbors.append(np.argmin(distance_mat, axis=0))
+        return a_neighbors, np.concatenate(b_neighbors, axis=0)
+
+
+class TorchNeighbors(Neighbors):
+    def neighbors(self, a, b):
         import torch
 
-        if torch.cuda.is_available():
-            return _nearest_neighbors_torch(
-                source, target, batch_size=batch_size, verbose=verbose
-            )
-    except ModuleNotFoundError:
-        pass
+        dev = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        source = torch.from_numpy(a).to(torch.float32).to(dev)
+        target = torch.from_numpy(b).to(source)
+        indices = torch.zeros([len(source)], device=dev, dtype=torch.int64)
+        distances = (torch.ones([len(source)]) * np.inf).to(source)
+        target_indices = []
+        batches = range(0, len(target), self.batch_size)
+        if self.verbose:
+            batches = tqdm(batches)
+        source_norms = torch.sum(source ** 2, dim=-1)[:, None]
+        for i in batches:
+            batch = target[i : i + self.batch_size]
+            target_norms = torch.sum(batch * batch, dim=-1)[None]
+            dots = source @ batch.T
+            distance_mat = source_norms + target_norms - 2 * dots
+            min_indices = torch.argmin(distance_mat, dim=-1)
+            min_values, _ = torch.min(distance_mat, dim=-1)
+            indices = torch.where(min_values < distances, min_indices + i, indices)
+            distances = torch.where(min_values < distances, min_values, distances)
+            target_indices.append(torch.argmin(distance_mat, dim=0).cpu().numpy())
+        return indices.cpu().numpy(), np.concatenate(target_indices, axis=0)
 
-    indices = np.zeros([len(source)], dtype=np.int)
-    distances = np.inf * np.ones([len(source)], dtype=source.dtype)
-    target_indices = []
-    batches = range(0, len(target), batch_size)
-    if verbose:
-        batches = tqdm(batches)
-    for i in batches:
-        batch = target[i : i + batch_size]
-        source_norms = np.sum(source * source, axis=-1)[:, None]
-        target_norms = np.sum(batch * batch, axis=-1)[None]
-        dots = source @ batch.T
-        distance_mat = source_norms + target_norms - 2 * dots
-        min_indices = np.argmin(distance_mat, axis=-1)
-        min_values = np.min(distance_mat, axis=-1)
-        indices = np.where(min_values < distances, min_indices + i, indices)
-        distances = np.minimum(distances, min_values)
-        target_indices.append(np.argmin(distance_mat, axis=0))
-    return indices, np.concatenate(target_indices, axis=0)
-
-
-def _nearest_neighbors_torch(source, target, batch_size=128, verbose=False):
-    import torch
-
-    dev = torch.device("cuda")
-    source = torch.from_numpy(source).to(torch.float32).to(dev)
-    target = torch.from_numpy(target).to(source)
-    indices = torch.zeros([len(source)], device=dev, dtype=torch.int64)
-    distances = (torch.ones([len(source)]) * np.inf).to(source)
-    target_indices = []
-    batches = range(0, len(target), batch_size)
-    if verbose:
-        batches = tqdm(batches)
-    for i in batches:
-        batch = target[i : i + batch_size]
-        source_norms = torch.sum(source * source, dim=-1)[:, None]
-        target_norms = torch.sum(batch * batch, dim=-1)[None]
-        dots = source @ batch.T
-        distance_mat = source_norms + target_norms - 2 * dots
-        min_indices = torch.argmin(distance_mat, dim=-1)
-        min_values, _ = torch.min(distance_mat, dim=-1)
-        indices = torch.where(min_values < distances, min_indices + i, indices)
-        distances = torch.where(min_values < distances, min_values, distances)
-        target_indices.append(torch.argmin(distance_mat, dim=0).cpu().numpy())
-    return indices.cpu().numpy(), np.concatenate(target_indices, axis=0)
--- voice_swap/ortho_udt_test.py
+++ voice_swap/ortho_udt_test.py
@@ -1,17 +1,20 @@
+import pytest
+
 import numpy as np
 
-from .ortho_udt import nearest_neighbors
+from .ortho_udt import NumpyNeighbors, TorchNeighbors
 
 
-def test_nearest_neighbors():
+@pytest.mark.parametrize("cls", [NumpyNeighbors, TorchNeighbors])
+def test_nearest_neighbors(cls):
     source = np.random.normal(size=(200, 3))
     target = np.random.normal(size=(200, 3))
-    source_neighbors, target_neighbors = nearest_neighbors(source, target)
+    source_neighbors, target_neighbors = cls().neighbors(source, target)
     for i, neighbor in enumerate(source_neighbors):
         actual = np.argmin(np.sum((source[i][None] - target) ** 2, axis=-1))
         assert neighbor == actual
     # Check target neighbors by making sure the operation
     # is symmetric.
-    target_neighbors_2, source_neighbors_2 = nearest_neighbors(target, source)
+    target_neighbors_2, source_neighbors_2 = cls().neighbors(target, source)
     assert (target_neighbors == target_neighbors_2).all()
     assert (source_neighbors == source_neighbors_2).all()
